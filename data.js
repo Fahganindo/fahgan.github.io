41614937328d88bf6a6a4065a6315bd615a64ad1
fahganindo <abdul.arafah2005@gmail.com>
2025-01-08 11:05:10 +0700

    Add files via upload
    Signed-off-by: fahganindo <abdul.arafah2005@gmail.com>

---
+++ data.js
@@ -0,0 +1,189 @@
+
+const teams = [ 
+    {
+        "name": "Muhammad Khaidir Fahram",
+        "image": "https://fahram.com//img/profile-photos/jzZj0jPuEKTWwY9a8lxo7pQhA29zxFpVQd7xl4BZ.jpg",
+        "email" : "fahram@fahram.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/khaidir.fahram"
+            },
+            {
+                "name" : "facebook",
+                "link" : "https://facebook.com/khaidir.fahram"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://fahram.com/"
+            }
+        ]
+    },
+    {
+        "name": "Achmad Maulana Ichsan",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "ichsanachmadmaulana@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/a.iichsan"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://icsan.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Marcella Setyana Putri",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "msetyaningputri@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/mqrcellaaaa"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://icsan.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Fredo Pradana",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "Fredopradana72@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/fredopradana"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://fredo16.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Rheina Wardiana Hafirah",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "rheinawardiana317@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/na_rrrrr44"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://icsan.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Muhammad Fikri Firdaus",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "mfikrif06@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/m_f1kr1"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://fikriy.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Ramdhani Rivanda",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "ramdhanirivanda@gmail.com ",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/ramdhanirivanda"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://fikriy.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Muhammad Sendi Nourman",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "sandinourman@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/sndiinrmn"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://sandinourman.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Rozak Al Baidawi",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "rozakalbaidawi@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/_rzkalbdwi"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://rozak19.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Naufal Laudza",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "naufallaudza194@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/naufalaudzaa"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://naufal19.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Hanif Dwi Novario",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "hanifdn05@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/hnfdwnvr"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://hanifdwi.my.id/"
+            }
+        ]
+    },
+    {
+        "name": "Abdulrahman Arafah Sunarto",
+        "image": "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+        "email" : "abdul.arafah2005@gmail.com",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/arafah.abdulrahman_"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://fahganindo4life.my.id/"
+            }
+        ]
+    },
+    
+    
+]
